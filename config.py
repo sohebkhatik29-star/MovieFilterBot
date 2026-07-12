c916de8c746238ba0acff3204f8bf2ace6ed181c
sohebkhatik29-star <sohebkhatik29@gmail.com>
2026-07-12 16:48:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-MONGO_URI = os.environ.get("MONGO_URI")
+API_ID = int(os.environ.get("30720676"))
+API_HASH = os.environ.get("a078e3476750afbd6db7d6c5e5e658d9")
+BOT_TOKEN = os.environ.get("8898921453:AAEsZ9uIAsyQQBsF5zlBJsMuPI8AWY5siUo")
+MONGO_URI = os.environ.get("mongodb+srv://sohebkhataik25:sohebkhataik25@cluster0.ewksnis.mongodb.net/?appName=Cluster0")
